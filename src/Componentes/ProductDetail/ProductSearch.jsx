a43ce0d85d80f872c93d59f33b2d900dafc38a53
pacomendieta <ff.mendieta@gmail.com>
2022-09-26 14:33:08 +0200

    Iniciada Pagina Home

---
+++ src/Componentes/ProductDetail/ProductSearch.jsx
@@ -0,0 +1,9 @@
+
+export const ProductSearchPage=({id,titulo})=>{
+return(
+
+
+
+    
+)
+}
---
+++ src/Componentes/Products/PaginaSearch.jsx
@@ -0,0 +1,5 @@
+export const PaginaSearch = ()=>{
+    return(
+        <h2>Página de búsqueda de Productos</h2>
+    )
+}
