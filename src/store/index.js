294489d93a604e48a374533ba2ac95a6293458bf
pacomendieta <ff.mendieta@gmail.com>
2022-09-29 14:29:33 +0200

    Añadido redux-devtools-extension

--- src/store/index.js
+++ src/store/index.js
@@ -2,12 +2,13 @@
 import {configureStore} from  '@reduxjs/toolkit';
 import {createStore} from 'redux';
 import productsReducer from './productos'
+import { composeWithDevTools } from 'redux-devtools-extension'; //Para que funcionen las devtools de redux
 
 // Crear Redux Store
 //const store = configureStore();
 
-
-let store = createStore ( productsReducer  );
+const store = createStore ( productsReducer, composeWithDevTools() ); 
+//let store = createStore ( productsReducer  );
 //let store = configureStore ( productsReducer  );
 
 export default store;
